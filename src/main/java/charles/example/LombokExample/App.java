108bbd04531f92c392710be51eae6869c1e2f37a
charles <sabaa35@gmail.com>
2017-07-23 12:12:36 +0800

    first commit

---
+++ src/main/java/charles/example/LombokExample/App.java
@@ -0,0 +1,16 @@
+package charles.example.LombokExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Customer c = new Customer();
+        c.setAge(11);
+        System.out.println(c.getAge());
+        
+    }
+}
---
+++ src/main/java/charles/example/LombokExample/Customer.java
@@ -0,0 +1,9 @@
+package charles.example.LombokExample;
+
+import lombok.Data;
+
+
+public @Data class Customer {
+	private String name;
+	private int age;
+}
